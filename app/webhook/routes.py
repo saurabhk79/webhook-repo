82a48f06056d888884e291953d123e8623e9fb24
Saurabh <imsauby@gmail.com>
2024-06-18 02:38:54 +0000

    added the request id to the code

--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -1,60 +1,50 @@
 from flask import Blueprint, request, jsonify, render_template
 from app.extensions import mongo
 import datetime
-import uuid
 
 webhook = Blueprint('webhook', __name__, url_prefix='/webhook')
-
 
 @webhook.route("/hello", methods=["GET"])
 def hello_route():
     return "Hello, world!"
 
-
 @webhook.route('/receiver', methods=["POST"])
 def receiver():
-    print("works", request.json)
     data = request.json
     event_type = request.headers.get('X-GitHub-Event')
-    request_id = str(uuid.uuid4())
-
-    print(event_type);
-    event = {
-        "_id": request_id,
-        "request_id": request_id,
-        "author": None,
-        "action": None,
-        "from_branch": None,
-        "to_branch": None,
-        "timestamp": datetime.datetime.now()
-    }
 
     if event_type == "push":
-        event.update({
-            "action": "PUSH",
+        event = {
+            "request_id": data['after'],
             "author": data['pusher']['name'],
+            "action": "PUSH",
+            "from_branch": None,
             "to_branch": data['ref'].split('/')[-1],
-        })
+            "timestamp": datetime.datetime.now()
+        }
     elif event_type == "pull_request":
-        event.update({
-            "action": "PULL_REQUEST",
+        event = {
+            "request_id": data['pull_request']['id'],
             "author": data['pull_request']['user']['login'],
+            "action": "PULL_REQUEST",
             "from_branch": data['pull_request']['head']['ref'],
             "to_branch": data['pull_request']['base']['ref'],
-        })
+            "timestamp": datetime.datetime.now()
+        }
     elif event_type == "merge":
-        event.update({
-            "action": "MERGE",
+        event = {
+            "request_id": data['pull_request']['id'],
             "author": data['sender']['login'],
+            "action": "MERGE",
             "from_branch": data['pull_request']['head']['ref'],
             "to_branch": data['pull_request']['base']['ref'],
-        })
+            "timestamp": datetime.datetime.now()
+        }
     else:
         return "Event not supported", 400
 
     mongo.db.events.insert_one(event)
     return jsonify({"message": "Success"}), 200
-
 
 @webhook.route('/events', methods=["GET"])
 def get_events():
@@ -62,7 +52,6 @@ def get_events():
     for event in events:
         event['_id'] = str(event['_id'])
     return jsonify(events)
-
 
 @webhook.route('/events/html', methods=["GET"])
 def get_events_html():
