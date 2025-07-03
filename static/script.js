4632b928c8716706228fce731aa159f9232e928c
Saurabh <imsauby@gmail.com>
2025-07-03 21:29:31 +0530

    updated the formatted ui values

--- static/script.js
+++ static/script.js
@@ -1,34 +1,39 @@
 function formatEvent(e) {
   let msg = "";
-  if (e.type === "push") {
+  const type = e.action;
+
+  if (type === "PUSH") {
     msg = `<span class="type">PUSH</span>: ${e.author} pushed to ${e.to_branch} on ${e.timestamp}`;
-  } else if (e.type === "pull_request") {
+  } else if (type === "PULL_REQUEST") {
     msg = `<span class="type">PR</span>: ${e.author} submitted a pull request from ${e.from_branch} to ${e.to_branch} on ${e.timestamp}`;
-  } else if (e.type === "merge") {
+  } else if (type === "MERGE") {
     msg = `<span class="type">MERGE</span>: ${e.author} merged branch ${e.from_branch} to ${e.to_branch} on ${e.timestamp}`;
   }
+
   return msg;
 }
 
 async function fetchEvents() {
-  const res = await fetch("/webhook/events");
-  const events = await res.json();
+  try {
+    const res = await fetch("/webhook/events");
+    const events = await res.json();
 
-  const container = document.getElementById("events");
-  container.innerHTML = "";
+    const container = document.getElementById("events");
+    container.innerHTML = "";
 
-  events.forEach((e) => {
-    const div = document.createElement("div");
-    div.className = "event";
-    div.innerHTML = formatEvent(e);
-    container.appendChild(div);
-  });
+    events.forEach((e) => {
+      const div = document.createElement("div");
+      div.className = "event";
+      div.innerHTML = formatEvent(e);
+      container.appendChild(div);
+    });
 
-  const now = new Date();
-  const timeStr = now.toLocaleTimeString();
-  document.getElementById(
-    "refresh-info"
-  ).textContent = `Last refreshed at: ${timeStr}`;
+    const now = new Date();
+    const timeStr = now.toLocaleTimeString();
+    document.getElementById("refresh-info").textContent = `Last refreshed at: ${timeStr}`;
+  } catch (err) {
+    console.error("Error fetching events:", err);
+  }
 }
 
 setInterval(fetchEvents, 15000);
